3e75e72d1b35087a9de5e9d7342a29afa85f7d44
lugalu <luizjaf01@gmail.com>
2023-02-02 21:20:42 -0300

    Feat: Added Firebase to the project

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 target 'SnapKitTest' do
+
   use_frameworks!
   pod 'SnapKit', '~> 5.6.0'
+  pod 'FirebaseAnalytics'
 
 end
--- SnapKitTest/ViewController.swift
+++ SnapKitTest/ViewController.swift
@@ -9,15 +9,33 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
+    var square: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel()
-        label.snp.makeConstraints{ make in
-            
-        }
+        self.makeUI()
     }
 
 
+}
+
+extension ViewController {
+    func makeUI(){
+        addViews()
+    }
+    
+    private func addViews(){
+        
+    }
+    
+    
+    
 }
 
