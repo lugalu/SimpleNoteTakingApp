f9af34fbe53f80a8eb04a6e89265857b7867406e
lugalu <luizjaf01@gmail.com>
2023-02-17 21:14:26 -0300

    Fix: File not saved in last commit, XCode git UI bug, fixed through terminal

--- SnapKitTest/FirebaseAccess/FireBaseMain.swift
+++ SnapKitTest/FirebaseAccess/FireBaseMain.swift
@@ -19,21 +19,52 @@ enum AccessType{
 class FirebaseMain: ServerAccess{
     private let databaseRef = Database.database().reference(withPath: "Notes")
     private var refObservers: [DatabaseHandle] = []
+    private var valueUpdated: BehaviorSubject<[Note]> = BehaviorSubject<[Note]>(value: [])
+    
+    init(){
+        databaseRef.observe(.value, with: { snapshots in
+            var newItens: [Note] = []
+            
+            for newItem in snapshots.children{
+                
+                guard let snapshot = newItem as? DataSnapshot,
+                      let newNote = Note.newFrom(snapshot: snapshot) else {
+                    continue
+                }
+                newItens.append(newNote)
+            }
+            
+            self.valueUpdated.onNext(newItens)
+            
+        })
+    }
     
     func requestData() -> Single<[Note]> {
         return Single.create{ _ in
             
+            
+            
             return Disposables.create()
         }
+        
+        
     }
     
-    func insertNew(Note note: Note) -> Result<Void, Error> {
-        return .success(())
+    func insertNew(Note note: Note){
+        let noteRef = self.databaseRef.child(note.id.uuidString)
+        noteRef.setValue(note.toAnyObject())
     }
     
-    func update(Note note: Note) -> Result<Void, Error> {
-        return .success(())
+    func update(Note note: Note){
+        let noteRef = self.databaseRef.child(note.id.uuidString)
+        noteRef.updateChildValues([
+            "title" : note.title,
+            "content": note.content
+        ])
     }
     
+    func noteObserver() -> BehaviorSubject<[Note]>{
+        return self.valueUpdated
+    }
     
 }
